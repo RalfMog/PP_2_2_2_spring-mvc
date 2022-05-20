98315b70d274e866a75d75826cda33e079a72c7e
RalfMog <104436602+RalfMog@users.noreply.github.com>
2022-05-20 20:45:29 +0500

    pom update

--- src/main/java/web/service/CarsServiceImpl.java
+++ src/main/java/web/service/CarsServiceImpl.java
@@ -3,22 +3,25 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.model.Car;
-import web.dao.CarsDao;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarsServiceImpl implements CarsService {
-    private final CarsDao carsDao;
-
-    @Autowired
-    public CarsServiceImpl(CarsDao carsDao) {
-        this.carsDao = carsDao;
-    }
-
+    private List<Car> carList = Arrays.asList(new Car("Audi", "black", 2022),
+            new Car("Mercedes-Benz", "green", 2020),
+            new Car("Honda", "blue", 2000),
+            new Car("Nissan", "green", 2012),
+            new Car("Ford", "grey", 2017));
     @Override
     public List<Car> carsCount(int number) {
-        return carsDao.carsCount(number);
+        List<Car> resultList = carList;
+        if (number == 0 || number > 5) {
+            return resultList;
+        }
+        return resultList.stream().limit(number).collect(Collectors.toList());
     }
 }
 
